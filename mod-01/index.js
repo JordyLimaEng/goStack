dedd03ab42c1d5ef895f26c09f9260e562591846
Jordy Lima <jordy.sousa@eng.ci.ufpb.br>
2020-09-09 09:51:31 -0300

    CRUD

--- mod-01/index.js
+++ mod-01/index.js
@@ -2,11 +2,27 @@ const express = require('express');
 
 const server = express();
 
+server.use(express.json()) //especifica para o node que os bodys das req's serão recebido em json
+
 // Query params = ?teste=1
 // Route params = /users/1
 // Request body = { "nome": "Jordy", "email": "jordylima51@gmail.com"}
 
-const users = ['Diego', 'Rafael', 'Flávio']
+// CRUD => Create Read Update Delete
+
+const users = ['Paulo', 'Rafael', 'Flávio']
+
+//Create
+server.post('/users', (req,res)=>{
+  const  {name} = req.body
+  users.push(name)
+  return res.json(users)
+})
+
+//Read
+server.get('/users', (req,res) => {
+  return res.json(users)
+})
 
 // localhost:3000/teste
 server.get('/users/:index', (req, res) => { //:id para especificar que é um route params
@@ -15,5 +31,22 @@ server.get('/users/:index', (req, res) => { //:id para especificar que é um rou
   
   return res.json(users[index])
 });
+
+//Put
+server.put('/users/:index', (req, res) =>{
+  const { index } = req.params
+  const { name } = req.body
+
+  users[index] = name
+
+  return res.json(users)
+})
+
+//Delete
+server.delete('/users/:index',(req,res) => {
+  const {index} = req.params
+  users.splice(index,1)
+  return res.send()
+})
 
 server.listen(3000); 

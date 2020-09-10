b50e792364c9a22662a0ed532f98eeaa2b3ac00a
Jordy Lima <jordy.sousa@eng.ci.ufpb.br>
2020-09-10 16:32:42 -0300

    suporte a upload de arquivos c/ multer

--- mod-02/src/routes.js
+++ mod-02/src/routes.js
@@ -2,8 +2,11 @@ import {Router} from 'express';
 import UserController from './app/controllers/UserController'
 import SessionController from './app/controllers/SessionController'
 import authMiddleware from './app/middlewares/auth'
+import multer from 'multer'
+import multerConfig from './config/multer'
 
 const routes = new Router();
+const upload = multer(multerConfig);
 
 routes.post('/users', UserController.store)
 routes.post('/sessions', SessionController.store)
@@ -11,6 +14,10 @@ routes.post('/sessions', SessionController.store)
 routes.use(authMiddleware);//so vale para as rotas após esse comando
 
 routes.put('/users', UserController.update)
+
+routes.post('/files', upload.single('file') ,(req, res) => {
+  return res.json({ok: true});
+})
 
 
 export default routes;

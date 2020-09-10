f26ac4ee695707f475fe3cd2197e6275d2dc5d4b
Jordy Lima <jordy.sousa@eng.ci.ufpb.br>
2020-09-10 14:13:18 -0300

    validaçao de campos com yup e token

--- mod-02/src/app/controllers/SessionController.js
+++ mod-02/src/app/controllers/SessionController.js
@@ -1,9 +1,17 @@
 import User from '../models/user'
 import jwt from 'jsonwebtoken'
 import authConfig from '../../config/auth'
+import * as Yup from 'yup'
 
 class SessionController{
   async store(req, res){
+
+    const schema = Yup.object().shape({
+      name: Yup.string().required(),
+      email: Yup.string().email().required(),
+      password: Yup.string().required().min(6),
+    })
+
     const {email, password} = req.body
     const user = await User.findOne({where: {email}})
 

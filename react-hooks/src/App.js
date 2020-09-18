55d63be72941180deab20fae896552dbf6eea42c
Jordy Lima <jordy.sousa@eng.ci.ufpb.br>
2020-09-18 12:01:47 -0300

    Introdução use state

--- react-hooks/src/App.js
+++ react-hooks/src/App.js
@@ -1,9 +1,31 @@
-import React from 'react';
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return <h1>Hello WOrld</h1>
+
+  const [tech, setTech] = useState([
+    'React',
+    'React Native'
+  ]);
+  const [newTech, setNewTech] = useState('');
+
+  function handleAdd() {
+    setTech([...tech, newTech]);
+    setNewTech('');
+  }
+
+  return (
+    <>
+      <ul>
+        {tech.map(t =>
+          <li key={t}>{t}</li>
+        )}
+      </ul>
+      <input type="text" value={newTech} onChange={e=>setNewTech(e.target.value)}/>
+      <button type="button" onClick={handleAdd}>Add tech</button>
+    </>
+  );
 }
 
 export default App;

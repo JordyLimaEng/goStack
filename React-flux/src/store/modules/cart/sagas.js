f178cd338e83608b49b47ef829e76e850e5aa8f9
Jordy Lima <jordy.sousa@eng.ci.ufpb.br>
2020-09-18 10:33:33 -0300

    Tratamento do Estoque no Carrinho
    Tratando alteração de estoque quando o produto está no carrinho

--- React-flux/src/store/modules/cart/sagas.js
+++ React-flux/src/store/modules/cart/sagas.js
@@ -4,7 +4,7 @@ import { toast } from 'react-toastify';
 import api from '../../../services/api';
 import { formatPrice } from '../../../util/format'
 
-import { addToCartSucess, updateAmount } from './actions'
+import { addToCartSuccess, updateAmountSuccess } from './actions'
 
 function* addToCart({ id }) {
   const productExists = yield select(
@@ -19,12 +19,12 @@ function* addToCart({ id }) {
   const amount = currentAmount + 1;
 
   if (amount > stockAmount) {
-      toast.error('Quantidade fora de estoque!');
-      return;
+    toast.error('Quantidade fora de estoque!');
+    return;
   }
 
   if (productExists) {
-    yield put(updateAmount(id, amount))
+    yield put(updateAmountSuccess(id, amount))
   } else {
     const response = yield call(api.get, `/products/${id}`);
 
@@ -33,11 +33,26 @@ function* addToCart({ id }) {
       amount: 1,
       priceFormatted: formatPrice(response.data.price),
     }
-    yield put(addToCartSucess(data));
+    yield put(addToCartSuccess(data));
+  }
+}
+
+function* updateAmount({ id, amount }) {
+  console.log("AAAAAA")
+  if (amount <= 0) return;
+
+  const stock = yield call(api.get, `/stock/${id}`);
+  const stockAmount = stock.data.amount;
+
+  if (amount > stockAmount) {
+    toast.error('Quantidade fora de estoque!');
+    return;
   }
 
+  yield put(updateAmountSuccess(id, amount));
 }
 
 export default all([
   takeLatest('@cart/ADD_REQUEST', addToCart),
+  takeLatest('@cart/UPDATE_AMOUNT_REQUEST', updateAmount),
 ]);

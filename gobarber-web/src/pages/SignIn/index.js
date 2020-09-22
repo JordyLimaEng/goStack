27738082bc1e9e94f0caecd001073b31c1a5bff5
Jordy Lima <jordy.sousa@eng.ci.ufpb.br>
2020-09-22 10:24:56 -0300

    Validando campos do auth

--- gobarber-web/src/pages/SignIn/index.js
+++ gobarber-web/src/pages/SignIn/index.js
@@ -1,10 +1,20 @@
 import React from 'react';
 import logo from '~/assets/logo.svg';
 import { Link } from 'react-router-dom';
-import { Form, Input} from '@rocketseat/unform';
+import { Form, Input } from '@rocketseat/unform';
+import * as Yup from 'yup';
+
+const schema = Yup.object().shape({
+  email: Yup.string()
+    .email("Insira um email válido!!")
+    .required("Email é obrigatório!"),
+  password: Yup.string()
+    .required("A senha é obrigatória!"),
+});
+
 
 function SignIn() {
-  function handleSubmit(data){
+  function handleSubmit(data) {
     console.log(data);
   }
 
@@ -12,7 +22,7 @@ function SignIn() {
     <>
       <img src={logo} alt="GoBarber" />
 
-      <Form onSubmit={handleSubmit}>
+      <Form schema={schema} onSubmit={handleSubmit}>
         <Input name="email" type="email" placeholder="email" />
         <Input name="password" type="password" placeholder="senha" />
 
--- gobarber-web/src/pages/SignUp/index.js
+++ gobarber-web/src/pages/SignUp/index.js
@@ -1,10 +1,22 @@
 import React from 'react';
 import logo from '~/assets/logo.svg';
 import { Link } from 'react-router-dom';
-import { Form, Input} from '@rocketseat/unform';
+import { Form, Input } from '@rocketseat/unform';
+import * as Yup from 'yup';
+
+const schema = Yup.object().shape({
+  name: Yup.string()
+    .required("Nome é obrigatório!"),
+  email: Yup.string()
+    .email("Insira um email válido!!")
+    .required("Email é obrigatório!"),
+  password: Yup.string()
+  .min(6, "No mínimo 6 caracteres!")
+    .required("A senha é obrigatória!"),
+});
 
 function SignUp() {
-  function handleSubmit(data){
+  function handleSubmit(data) {
     console.log(data);
   }
 
@@ -12,7 +24,7 @@ function SignUp() {
     <>
       <img src={logo} alt="GoBarber" />
 
-      <Form onSubmit={handleSubmit}>
+      <Form schema={schema} onSubmit={handleSubmit}>
         <Input name="name" placeholder="Nome Completo" />
         <Input name="email" type="email" placeholder="email" />
         <Input name="password" type="password" placeholder="senha" />

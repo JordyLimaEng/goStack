f97c081e9ed10deb0b7a7a5beff1f491b4d45e65
Jordy Lima <jordy.sousa@eng.ci.ufpb.br>
2020-09-22 16:54:51 -0300

    corrigindo erro parametros do compose

--- gobarber-web/src/store/createStore.js
+++ gobarber-web/src/store/createStore.js
@@ -1,9 +1,8 @@
 import { createStore, compose, applyMiddleware } from 'redux';
 
 export default (reducers, middlewares) => {
-  const enhancer = process.env.NODE_ENV === 'devlopment'
-    ? compose(
-      console.tron.createEnhancer().
+  const enhancer = process.env.NODE_ENV === 'development'
+    ? compose(console.tron.createEnhancer(),
       applyMiddleware(...middlewares)
     )
     : applyMiddleware(...middlewares);

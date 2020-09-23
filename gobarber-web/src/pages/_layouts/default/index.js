b2beec7f4cb0e0eb114d3a65b2716885f892f89f
Jordy Lima <jordy.sousa@eng.ci.ufpb.br>
2020-09-23 15:18:38 -0300

    Configuração Header

--- gobarber-web/src/pages/_layouts/default/index.js
+++ gobarber-web/src/pages/_layouts/default/index.js
@@ -2,9 +2,11 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { Wrapper } from './styles';
 
+import Header from '~/components/Header';
 export default function DefaultLayout({ children }) {
   return (
     <Wrapper>
+      <Header />
       {children}
     </Wrapper>
   );

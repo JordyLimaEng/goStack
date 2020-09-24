6e2bcdd5d3984d1c314c5fc50d6785e55bab9d7b
Jordy Lima <jordy.sousa@eng.ci.ufpb.br>
2020-09-24 14:05:54 -0300

    Dados do header

--- gobarber-web/src/components/Header/index.js
+++ gobarber-web/src/components/Header/index.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
 import { Link } from 'react-router-dom';
 
 import Notifications from '~/components/Notifications';
@@ -7,6 +8,8 @@ import logo from '~/assets/logo2.svg';
 import { Container, Content, Profile } from './styles';
 
 function Header() {
+  const profile = useSelector(state => state.user.profile);
+
   return (
     <Container>
       <Content>
@@ -16,16 +19,17 @@ function Header() {
         </nav>
 
         <aside>
-          <Notifications/>
+          <Notifications />
           <Profile>
             <div>
-              <strong>Diego cabra safado</strong>
+              <strong>{profile.name}</strong>
               <Link to="/profile">Meu Perfil</Link>
             </div>
-            <img src="https://api.adorable.io/avatars/50/abott@adorable.png" alt="Diego Fernandes" />
+            <img src={profile.avatar.url || "https://api.adorable.io/avatars/50/abott@adorable.png"} alt="" />
           </Profile>
         </aside>
       </Content>
+
     </Container>
   );
 }

196c8529a784b8843197256b7863881fcafd36b6
Jordy Lima <jordy.sousa@eng.ci.ufpb.br>
2020-09-24 11:09:06 -0300

    Atualizar Perfil

--- gobarber-web/src/store/modules/user/sagas.js
+++ gobarber-web/src/store/modules/user/sagas.js
@@ -1,3 +1,30 @@
-import {all} from 'redux-saga/effects';
+import { takeLatest, call, put, all } from 'redux-saga/effects';
+import { toast } from 'react-toastify';
 
-export default all([])
+import api from '~/services/api';
+
+import { updateProfileSuccess, updateProfileFailure } from './actions';
+
+export function* updateProfile({ payload }) {
+  try {
+    const { name, email, ...rest } = payload.data;
+    
+    const profile = Object.assign(
+      { name, email },
+      rest.oldPassword ? rest : {}
+    );
+    
+    const response = yield call(api.put, 'users', profile);
+
+    toast.success('Perfil atualizado!');
+
+    yield put(updateProfileSuccess(response.data));
+  } catch (err) {
+    toast.success('erro ao atualizar perfil, por favor verifique os dados!');
+    yield put(updateProfileFailure());
+  }
+}
+
+export default all([
+  takeLatest('@user/UPDATE_PROFILE_REQUEST', updateProfile)
+]);

d6700e27853a2cf83849553a962c32f26915d10f
Jordy Lima <jordy.sousa@eng.ci.ufpb.br>
2020-09-24 14:33:00 -0300

    Estilização da página de dashboard

--- gobarber-web/src/pages/Dashboard/index.js
+++ gobarber-web/src/pages/Dashboard/index.js
@@ -1,9 +1,41 @@
 import React from 'react';
 import api from '~/services/api'
 
+import { MdChevronLeft, MdChevronRight } from 'react-icons/md'
+
+import { Container, Time } from './styles';
+
 function Dashboard() {
-  api.get('appointments');
-  return <h1>Dashboard</h1>;
+  return <Container>
+    <header>
+      <button type="button">
+        <MdChevronLeft size={36} color="#FFF" />
+      </button>
+      <strong>31 de maio</strong>
+      <button type="button">
+        <MdChevronRight size={36} color="#FFF" />
+      </button>
+    </header>
+
+    <ul>
+      <Time past>
+        <strong>8:00</strong>
+        <span>JACA LIMNTA</span>
+      </Time>
+      <Time available>
+        <strong>09:00</strong>
+        <span>Em aberto</span>
+      </Time>
+      <Time>
+        <strong>10:00</strong>
+        <span>JACA LIMNTA</span>
+      </Time>
+      <Time>
+        <strong>11:00</strong>
+        <span>JACA LIMNTA</span>
+      </Time>
+    </ul>
+  </Container>;
 }
 
 export default Dashboard;
